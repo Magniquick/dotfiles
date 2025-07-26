1da2c0b1b5ed38aba2c0cc284eedb3f876d614ca
Magniquick <67406856+Magniquick@users.noreply.github.com>
2025-07-26 14:55:46 +0530

    sync files

--- dot_config/hypr/lock.sh
+++ dot_config/hypr/lock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-hyprlock && systemctl --user start hyprunlock-mode.service && systemctl --user disable --now hyprlock-mode.service
+systemctl --user start hyprlock.service && systemctl --user start hyprunlock-mode.service && systemctl --user disable --now hyprlock-mode.service
