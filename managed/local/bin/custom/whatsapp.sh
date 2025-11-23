a49afa1e87aa23c85e812568887d721fd44f9d27
Magniquick <67406856+Magniquick@users.noreply.github.com>
2025-11-23 23:58:04 +0530

    cooler waybar

--- managed/local/bin/custom/whatsapp.sh
+++ managed/local/bin/custom/whatsapp.sh
@@ -2,9 +2,8 @@
 
 # grab the address (will be empty if no matching window)
 addr=$(hyprctl clients -j | jq -r 'limit(1; .[] 
-	| select(.title | test("web\\.whatsapp\\.com")) 
+	| select(.initialTitle | test("web.whatsapp.com_/")) 
 	| .address)')
-
 if [[ -n "$addr" ]]; then
 	# $addr is not empty -> we found a matching client
 	hyprctl dispatch focuswindow address:"$addr"
